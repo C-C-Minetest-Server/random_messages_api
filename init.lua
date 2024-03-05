bcd13a34dea2ba39f62a97292b6c0dd36f0bd902
1F616EMO <root@1f616emo.xyz>
2024-03-05 19:31:37 +0800

    settings: Send message without any players online

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ random_messages_api = {
 }
 
 function random_messages_api.register_message(msg)
-    random_messages_api.list[#random_messages_api.list+1] = msg
+    random_messages_api.list[#random_messages_api.list + 1] = msg
 end
 
 function random_messages_api.from_table(tab)
@@ -42,7 +42,7 @@ function random_messages_api.from_file(filename)
     end
     repeat
         local value = file:read("*l")
-        if value and value ~= "" and string.sub(value,1,1) ~= "#" then
+        if value and value ~= "" and string.sub(value, 1, 1) ~= "#" then
             random_messages_api.register_message(value)
         end
     until value == nil
@@ -51,17 +51,20 @@ function random_messages_api.from_file(filename)
 end
 
 function random_messages_api.pick_message()
-    if #random_messages_api.list == 0 then return nil
-    elseif #random_messages_api.list == 1 then return random_messages_api.list[1] end
+    if #random_messages_api.list == 0 then
+        return nil
+    elseif #random_messages_api.list == 1 then
+        return random_messages_api.list[1]
+    end
     local id
     repeat
-        id = math.random(1,#random_messages_api.list)
+        id = math.random(1, #random_messages_api.list)
     until id ~= random_messages_api.last_picked
     random_messages_api.last_picked = id
     return random_messages_api.list[id]
 end
 
-if minetest.settings:get_bool("random_messages_api.load_custom_messages",true) then
+if minetest.settings:get_bool("random_messages_api.load_custom_messages", true) then
     local WP = minetest.get_worldpath()
     if not random_messages_api.from_file(WP .. DIR_DELIM .. "random_messages.txt") then
         minetest.log("warning", "[random_messages_api] Load from random_messages.txt failed")
@@ -69,14 +72,15 @@ if minetest.settings:get_bool("random_messages_api.load_custom_messages",true) t
 end
 
 local function loop()
-    local msg = random_messages_api.pick_message()
-    if msg then
-        minetest.chat_send_all(minetest.get_color_escape_sequence("grey") .. msg)
-        minetest.log("action", "[random_messages_api] MSG: " .. minetest.get_translated_string("en", msg))
+    if minetest.settings:get_bool("random_messages_api.send_without_players", true)
+        or #minetest.get_connected_players() ~= 0 then
+        local msg = random_messages_api.pick_message()
+        if msg then
+            minetest.chat_send_all(minetest.get_color_escape_sequence("grey") .. msg)
+            minetest.log("action", "[random_messages_api] MSG: " .. minetest.get_translated_string("en", msg))
+        end
     end
     minetest.after(random_messages_api.interval, loop)
 end
 
 minetest.after(random_messages_api.interval, loop)
-
-
